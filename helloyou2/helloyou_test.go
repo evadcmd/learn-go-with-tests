8b3441c9e73d31cfe82b8290c61eb067d453fa98
m-chiu <m-chiu@XW-000341noMacBook-Pro.local>
2021-10-06 02:25:07 +0900

    add a t.Run example

--- helloyou2/helloyou_test.go
+++ helloyou2/helloyou_test.go
@@ -3,24 +3,14 @@ package main
 import "testing"
 
 func TestSayHelloFunc(t *testing.T) {
-	/*
-		names := [...]string{
-			"TATA",
-			"RURU",
-			"chibi",
-			"",
+
+	assertEqual := func(t *testing.T, got string, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
 		}
-		for _, name := range names {
-			if name == "" {
-				name = "world"
-			}
-			want := "Hello, " + name
-			got := sayhello(name)
-			if got != want {
-				t.Errorf("got %s want %s", got, want)
-			}
-		}
-	*/
+	}
+
 	t.Run("saying hello to people", func(t *testing.T) {
 		names := [...]string{
 			"TATA",
@@ -30,17 +20,13 @@ func TestSayHelloFunc(t *testing.T) {
 		for _, name := range names {
 			want := "Hello, " + name
 			got := sayhello(name)
-			if got != want {
-				t.Errorf("got %s want %s", got, want)
-			}
+			assertEqual(t, got, want)
 		}
 	})
 
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
 		want := "Hello, world"
 		got := sayhello("")
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertEqual(t, got, want)
 	})
 }

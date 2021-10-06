ed8bb362009375fbb0ebe72ec9bfeb20769dac88
m-chiu <m-chiu@XW-000341noMacBook-Pro.local>
2021-10-06 14:31:44 +0900

    add && refractor some examples

--- perimeter2/perimeter_test.go
+++ perimeter2/perimeter_test.go
@@ -18,44 +18,54 @@ type Shape interface {
 
 func TestPeri(t *testing.T) {
 	tests := []struct {
+		name      string
 		shape     Shape
 		perimeter float64
 	}{
-		{shape: &Rect{}, perimeter: 0},
-		{shape: &Rect{5, 2}, perimeter: 14},
-		{shape: &Rect{5.2, 2.5}, perimeter: 2 * (5.2 + 2.5)},
-		{shape: &Circle{5}, perimeter: 2 * math.Pi * 5},
-		{shape: &Circle{2.5}, perimeter: 2 * math.Pi * 2.5},
+		{name: "Rect1", shape: &Rect{}, perimeter: 0},
+		{name: "Rect2", shape: &Rect{5, 2}, perimeter: 14},
+		{name: "Rect3", shape: &Rect{5.2, 2.5}, perimeter: 2 * (5.2 + 2.5)},
+		{name: "Circ1", shape: &Circle{5}, perimeter: 2 * math.Pi * 5},
+		{name: "Circ2", shape: &Circle{2.5}, perimeter: 2 * math.Pi * 2.5},
 	}
 
 	for _, test := range tests {
-		got := test.shape.Perimeter()
-		want := test.perimeter
-		t.Logf("got %v want %v\n", got, want)
-		if !floatEqual(got, want) {
-			t.Errorf("got %v want %v\n", got, want)
-		}
+		// run a single test
+		// go test -run ${FUNC_NAME}/${test.name}
+		t.Run(test.name, func(t *testing.T) {
+			got := test.shape.Perimeter()
+			want := test.perimeter
+			t.Logf("got %v want %v\n", got, want)
+			if !floatEqual(got, want) {
+				t.Errorf("got %v want %v\n", got, want)
+			}
+		})
 	}
 }
 
 func TestArea(t *testing.T) {
 	tests := []struct {
+		name  string
 		shape Shape
 		area  float64
 	}{
-		{shape: &Rect{}, area: 0},
-		{shape: &Rect{5, 2}, area: 10},
-		{shape: &Rect{5.2, 2.5}, area: 5.2 * 2.5},
-		{shape: &Circle{5}, area: math.Pi * 5 * 5},
-		{shape: &Circle{2.5}, area: math.Pi * 2.5 * 2.5},
+		{name: "Rect1", shape: &Rect{}, area: 0},
+		{name: "Rect2", shape: &Rect{5, 2}, area: 10},
+		{name: "Rect3", shape: &Rect{5.2, 2.5}, area: 5.2 * 2.5},
+		{name: "Circ1", shape: &Circle{5}, area: math.Pi * 5 * 5},
+		{name: "Circ2", shape: &Circle{2.5}, area: math.Pi * 2.5 * 2.5},
 	}
 
 	for _, test := range tests {
-		got := test.shape.Area()
-		want := test.area
-		t.Logf("got %v want %v\n", got, want)
-		if !floatEqual(got, want) {
-			t.Errorf("got %v want %v\n", got, want)
-		}
+		// run a single test
+		// go test -run ${FUNC_NAME}/${test.name}
+		t.Run(test.name, func(t *testing.T) {
+			got := test.shape.Area()
+			want := test.area
+			t.Logf("got %v want %v\n", got, want)
+			if !floatEqual(got, want) {
+				t.Errorf("got %v want %v\n", got, want)
+			}
+		})
 	}
 }
